55bbe70de77583e7a585dad93ada99484d1f355c
nhat <nhatthuynhh621@gmail.com>
2023-02-14 17:14:44 +0700

    [UI/Logic] add login form, register form popup at main screen; Send form data to backend

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import express from "express"
+import bodyParser from 'body-parser'
+
+import routes from "./routes/index.js"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+// app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use('/api/v1', routes)
 app.get('/', (req, res) => {
     res.render('pages/index')
 })
