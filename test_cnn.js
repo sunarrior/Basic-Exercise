8072604d782639043bee152d495551da036f6cb0
nhat <nhatthuynhh621@gmail.com>
2023-02-24 15:50:04 +0700

    config cronjob

--- test_cnn.js
+++ test_cnn.js
@@ -1,11 +1,4 @@
-import util from "util";
-const { generatePrime } = await import("node:crypto");
+import cronjob from "./utils/cronjob.helper.js";
 
-const generatePrimePromise = util.promisify(generatePrime);
-let p = await generatePrimePromise(64, { bigint: true });
-let q = await generatePrimePromise(64, { bigint: true });
-
-// console.log(p);
-const n = p * q;
-
-console.log(n.toString(16));
+cronjob.test();
+console.log("end main");
